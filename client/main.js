a6d44c1b5ff5a28d28627214de94550bd4e55b9a
Antonin Adert <antonin.adert@gmail.com>
2017-12-06 21:43:49 +0100

    added universal-router/legqcy (because old version of Node with Meteor 1.5.x

--- client/main.js
+++ client/main.js
@@ -1,7 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 import { hydrate } from 'react-dom';
 import React from 'react'; //React needed for the <Component /> Syntax
-import App from '../imports/App';
+import App from '/imports/App';
+import UniversalRouter from 'universal-router';
+import createHistory from 'history/createBrowserHistory'
+
+const routes = [
+    {
+      path: '',
+      action() {
+        return {
+          component:  <App />
+        };
+      }
+    }]
+    const History = createHistory()
+    const location = History.location
+    const router = new UniversalRouter(routes);
+
+    function renderLocation(location) {
+        router.resolve({pathname: location.pathname}).then(route  => { 
+          //route is what our action() returns for a specific path
+          document.title = route.title;
+          hydrate(route.component, document.getElementById("app"));
+        });
+      }
+      
+      //Initialize the first page render with current History
+      renderLocation(location);
+      //listen to URL(location) changes and render the new layout based on URL automatically
+      History.listen((anylocation) => {renderLocation(anylocation)});
+
+
 Meteor.startup(() => {
-    hydrate(<App />, document.getElementById("app"));
 });
--- server/main.js
+++ server/main.js
@@ -1,16 +1,28 @@
 import { Meteor } from 'meteor/meteor';
+import UniversalRouter from 'universal-router/legacy';
 import { renderToString } from "react-dom/server";
 import { onPageLoad } from "meteor/server-render";
 import React from 'react'; //React needed for the <Component /> Syntax
 import App from '../imports/App';
 
-function renderPage(thatSink) {
-    thatSink.renderIntoElementById("app", renderToString(
-      <App />
-    ));
-    const title = "<title>"+'SSR TEST'+"</title>";
-    thatSink.appendToHead(title);
-}
+const routes = [
+  {
+    path: '',
+    action() {
+      return {
+        component:  <App />
+      };
+    }
+  }]
+  const router = new UniversalRouter(routes);
+
+  function renderPage(thatSink) {
+    router.resolve({pathname: thatSink.request.url.pathname}).then(route  => {
+      thatSink.renderIntoElementById("app", renderToString(
+        route.component
+      ));
+    });
+  }
 
 onPageLoad(async sink => {
   await renderPage(sink);
